fe6b6b95b22294627cc1cce11f7fb83127fc8edb
Mark8301 <38378908+Mark8301@users.noreply.github.com>
2018-09-29 11:52:27 -0400

    Add files via upload

---
+++ sketch_sep29a.ino
@@ -0,0 +1,18 @@
+int wait=2000;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13,HIGH);
+  delay(wait);
+  digitalWrite(13,LOW);
+  delay(wait);
+  if(wait>100){
+    wait=wait-100;
+  } 
+  
+}
