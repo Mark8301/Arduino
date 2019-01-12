e1c441c4fb766b9aa5177480b490a0523271ec34
Mark8301 <38378908+Mark8301@users.noreply.github.com>
2019-01-12 14:59:16 -0500

    Add files via upload

---
+++ BlinkingLight.ino
@@ -0,0 +1,129 @@
+void setup() {
+  int ledPin = 13;
+  pinMode(ledPin,OUTPUT); 
+  
+}
+
+
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int ledPin = 13;
+  
+  digitalWrite(ledPin,HIGH);
+  delay(500);
+  digitalWrite(ledPin,LOW);
+  delay(500);
+  
+  digitalWrite(ledPin,HIGH);
+  delay(500);
+  digitalWrite(ledPin,LOW);
+  delay(500);  
+
+   digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);  
+
+   digitalWrite(ledPin,HIGH);
+  delay(500);
+  digitalWrite(ledPin,LOW);
+  delay(500);
+
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);  
+
+  digitalWrite(ledPin,HIGH);
+  delay(500);
+  digitalWrite(ledPin,LOW);
+  delay(500);  
+
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);  
+
+  digitalWrite(ledPin,HIGH);
+  delay(500);
+  digitalWrite(ledPin,LOW);
+  delay(500);
+
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);  
+
+  digitalWrite(ledPin,HIGH);
+  delay(500);
+  digitalWrite(ledPin,LOW);
+  delay(500);
+
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);
+
+  digitalWrite(ledPin,HIGH);
+  delay(500);
+  digitalWrite(ledPin,LOW);
+  delay(500);
+
+  digitalWrite(ledPin,HIGH);
+  delay(500);
+  digitalWrite(ledPin,LOW);
+  delay(500);
+
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);
+
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);
+
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);  
+
+  digitalWrite(ledPin,HIGH);
+  delay(500);
+  digitalWrite(ledPin,LOW);
+  delay(500);  
+
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);  
+
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);
+  
+  digitalWrite(ledPin,HIGH);
+  delay(500);
+  digitalWrite(ledPin,LOW);
+  delay(500);
+  
+  digitalWrite(ledPin,HIGH);
+  delay(500);
+  digitalWrite(ledPin,LOW);
+  delay(500);
+  
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);
+
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(100);
+}
